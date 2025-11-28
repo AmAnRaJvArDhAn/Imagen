a3caf2e0fcbf560e4076128ea6523ee5caf39c26
Aman Raj <amanr0883@gmail.com>
2025-11-28 22:09:28 +0530

    updated backend and frontend

--- Backend/Routes/imageRoutes.js
+++ Backend/Routes/imageRoutes.js
@@ -49,7 +49,7 @@ router.post("/generate", auth, async (req, res) => {
     console.log("Prompt:", prompt);
 
     // Select a model
-    const selectedModel = model || FLUX_MODELS[1];
+    const selectedModel = model || FLUX_MODELS[4];
 
     console.log("Selected model:", selectedModel);
 
